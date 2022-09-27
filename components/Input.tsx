24ac584dadb57c2ce67bb6abbb27fa4058b4cfa0
Andres Goncalves <88066799+andresgoncalves@users.noreply.github.com>
2022-09-27 16:57:32 -0400

    Fixed Input component label

--- components/Input.tsx
+++ components/Input.tsx
@@ -8,12 +8,12 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 
 export default function Input({ label, icon, className, ...rest }: InputProps) {
   return (
-    <div className={`flex flex-col gap-1 ${className}`}>
-      <label className="text-xs">{label}</label>
+    <label className={`flex flex-col gap-1 ${className}`}>
+      <div className="text-xs">{label}</div>
       <div className="flex items-center border-b-primary-gray-500 border-b p-2 focus-within:border-b-primary-green-500">
         {icon}
         <input className="border-0" {...rest} />
       </div>
-    </div>
+    </label>
   )
 }
